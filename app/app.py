cf7d80e731709ba2255cd0a111c2c7446b1077b5
Víctor Hugo Valle Castillo <victouk@gmail.com>
2026-01-05 23:45:15 -0500

    feat: add endpoints to upload and get files

--- app/app.py
+++ app/app.py
@@ -1,6 +1,16 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, Query, status
+from fastapi import (
+    Depends,
+    FastAPI,
+    File,
+    Form,
+    HTTPException,
+    Query,
+    UploadFile,
+    status,
+)
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import Post, create_db_and_tables, get_async_session
@@ -110,3 +120,53 @@ def get_post(id: int):
         raise HTTPException(status_code=404, detail="Post not found.")
 
     return text_posts.get(id)
+
+
+@app.post(
+    "/files/upload",
+    summary="Upload a file with a caption.",
+    tags=["Files"],
+)
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(...),
+    session: AsyncSession = Depends(get_async_session),
+):
+    new_post = Post(
+        caption=caption,
+        url=f"/files/{file.filename}",
+        file_type=file.content_type,
+        file_name=file.filename,
+    )
+    session.add(new_post)
+    await session.commit()
+    await session.refresh(new_post)
+    return {"id": new_post.id, "caption": new_post.caption, "url": new_post.url}
+
+
+@app.get(
+    "/files",
+    summary="Get a list of uploaded files.",
+    tags=["Files"],
+)
+async def get_files(
+    session: AsyncSession = Depends(get_async_session),
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+            }
+        )
+    return {
+        "posts": posts_data,
+    }
